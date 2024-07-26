f84e7ebd5caca1c1838ff59d200c8a7f48677af1
François Roget <francois.roget@paynovate.com>
2024-07-26 18:04:24 +0200

    Another try

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
+	basePath: '', output: 'export',
 	distDir: './dist',
 	images: {
 		unoptimized: true,
@@ -14,5 +14,4 @@ const nextConfig = {
 		],
 	},
 };
-
 export default nextConfig;
